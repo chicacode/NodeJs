4378f55d3a89117508d428f6cf6f40a9349ac522
Geraldine <gerahernan.al@gmail.com>
2023-05-09 14:34:51 -0700

    feat: adding reouting

--- lab-2/router.js
+++ lab-2/router.js
@@ -1,8 +1,19 @@
 const handleRequest = (req, res) => {
     switch(req.url){
         case '/':
-            res.writeHead(200, { 'Content-type': 'text/plain' });
-            res.write('Hello Geri Node')
+            res.writeHead(200, { 'Content-type': 'text/html' });
+            res.write(`
+            <html>
+            <head><title>Hello Node!</title></head>
+                <body>
+                <div>
+                    <h1>Hello Node Geri</h1>
+                    <a href="http://localhost:8000/read-message">Read Message</a>
+                    <a href="http://localhost:8000/write-message">Write Message</a>
+                </div>
+                </body>
+            </html>
+        `)
             res.end();
             break;
         case '/read-message':
