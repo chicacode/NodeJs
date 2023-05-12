ae84ee5d71cf80d36aba89ffdf674ed26272792b
chicacode <gerahernan.al@gmail.com>
2023-05-11 22:15:50 -0700

    feat: adding templates ejs and setting ejs in index

--- lab-4/src/index.js
+++ lab-4/src/index.js
@@ -9,6 +9,8 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
 /* ------------------------------- middleware ------------------------------- */
 app.use('/api/members', require('./routes/members'))
