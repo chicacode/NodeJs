11381fe02080b04c34229046a170ac97bccb8aaa
chicacode <gerahernan.al@gmail.com>
2023-05-19 13:55:34 -0700

    feat: wip unnecesary files

--- lab-6/recipes-project/routes/recipe.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { getAllRecipe, getAddRecipePage, postAddRecipe } = require('../controllers/recipe.controller')
-
-router.get('/', getAllRecipe)
-
-router.route('/save').get(getAddRecipePage).post(postAddRecipe)
-// router.post('/save', postAddRecipe)
-
-module.exports = router
--- lab-6/recipes-project/utils/path-helper.js
+++
@@ -1,4 +0,0 @@
-//older way
-// process.mainModule.filename
-//newer way
-module.exports = require.main.path
