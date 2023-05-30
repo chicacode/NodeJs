b2dee77e423476cad2735e2b4af3804718b01a4a
chicacode <gerahernan.al@gmail.com>
2023-05-30 14:20:29 -0700

    feat: added routes get post login logout

--- auth-sessions-cookies/src/index.js
+++ auth-sessions-cookies/src/index.js
@@ -1,12 +1,20 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import session from "express-session";
+const express = require('express');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 
 const app = express();
+const PORT = 4000;
+// cookie parser middleware
 app.use(cookieParser());
-const oneDay = 1000 * 60 * 60 * 24;
+// parsing the incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//serving public file
 
-app.use(session({
+// Creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+//session middleware
+app.use(sessions({
     secret: 'this-is-my-super-mega-secret-blabla',
     saveUninitialized: true,
     cookie: { maxAge: oneDay },
@@ -15,8 +23,46 @@ app.use(session({
 
 
 
-app.listen(8000, () =>{
-    console.log("Port listenning in 8000")
+const myusername = "user1";
+const mypassword = "password";
+// a variable to save a session
+var session;
+
+app.use(express.static(__dirname + '/views'))
+// console.log(__dirname)
+
+
+//Add Routes
+app.get('/', (req, res) =>{
+    session = req?.session;
+    if(session.userid){
+        res.send("Welcome User <a href=\'/logout'>click to logout</a>")
+    }else{
+        res.sendFile('/views/index.html',{root:__dirname})
+    }
+});
+
+app.post('/user', (req, res) => {
+    if(req.body.username == myusername && req.body.password == mypassword){
+        console.log("entra en IF POST")
+        session = req.session;
+        session.userid = req.body.username;
+        console.log(req.session);
+        res.send(`Hey there, Welcome! <a href=\'/logout'>click to logout</a>`);
+    }else{
+        console.log("entra en ELSE")
+        res.send("Invalid username or password, try again!")
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is runnng on PORT: ${PORT}`)
 });
 
 
